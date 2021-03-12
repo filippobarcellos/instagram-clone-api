32a6b4d8f2ae3d1c912e69647b0a532a1080a241
Filippo Barcellos <barcellos.filippo@gmail.com>
2021-03-12 16:22:52 +0000

    fix heroku url

--- src/index.js
+++ src/index.js
@@ -11,4 +11,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT, () => console.log('server is running'));
-//postgres://qpvswfridoytvt:19e712ce8d60843fb87cbde68f4f33cb550c6c068d1dd7caf8e72f3c78160125@ec2-52-71-231-37.compute-1.amazonaws.com:5432/d7e1bbgepam6lr"
